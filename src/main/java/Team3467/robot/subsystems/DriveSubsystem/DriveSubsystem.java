19e7c5415b1c5a592c2a8fea41f709ad1e4eab1d
Abe Panteli <apanteli23@team3467.org>
2022-01-08 11:44:28 -0500

    Changed WPI_TalonFX to TalonFx

--- src/main/java/Team3467/robot/subsystems/DriveSubsystem/DriveSubsystem.java
+++ src/main/java/Team3467/robot/subsystems/DriveSubsystem/DriveSubsystem.java
@@ -5,34 +5,22 @@
 package Team3467.robot.subsystems.DriveSubsystem;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import Team3467.robot.Constants;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   
-  private final WPI_TalonFX m_rightDrive1 = new WPI_TalonFX(Constants.CanConstants.kdriveMotorRight1);
-  private final WPI_TalonFX m_rightDrive2 = new WPI_TalonFX(Constants.CanConstants.kdriveMotorRight2);
-  private final WPI_TalonFX m_leftDrive1 = new WPI_TalonFX(Constants.CanConstants.kdriveMotorLeft1);
-  private final WPI_TalonFX m_leftDrive2 = new WPI_TalonFX(Constants.CanConstants.kdriveMotorLeft2);
+  private final TalonFX m_rightDrive1 = new TalonFX(Constants.CanConstants.kdriveMotorRight1);
+  private final TalonFX m_rightDrive2 = new TalonFX(Constants.CanConstants.kdriveMotorRight2);
+  private final TalonFX m_leftDrive1 = new TalonFX(Constants.CanConstants.kdriveMotorLeft1);
+  private final TalonFX m_leftDrive2 = new TalonFX(Constants.CanConstants.kdriveMotorLeft2);
 
-  private final DifferentialDrive m_drive;
-
-  MotorControllerGroup m_rightControllers;
-  MotorControllerGroup m_leftControllers;
-  
   public DriveSubsystem() {
 
     m_leftDrive1.setInverted(true);
     m_leftDrive2.setInverted(true);    
-
-    m_rightControllers = new MotorControllerGroup(m_rightDrive1, m_rightDrive2);
-    m_leftControllers = new MotorControllerGroup(m_rightDrive1, m_rightDrive2);
-    
-    m_drive = new DifferentialDrive(m_leftControllers, m_rightControllers);
 
     m_leftDrive1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
     m_leftDrive2.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
@@ -43,11 +31,6 @@ public class DriveSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-
-  public void arcadeDrive(double fwd, double rot)
-  { 
-    m_drive.arcadeDrive(fwd, rot);
   }
 
   public double getAvgEncoder(){
