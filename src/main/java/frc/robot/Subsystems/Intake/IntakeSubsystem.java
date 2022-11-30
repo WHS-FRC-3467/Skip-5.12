f9e06f2286fb1a787eb9b17d39e8a4c59bd94f0b
DS and Master Programmer <apanteli23@team3467.org>
2022-11-29 20:54:18 -0500

    removed one intake motor

--- src/main/java/frc/robot/Subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeSubsystem.java
@@ -6,7 +6,6 @@
 package frc.robot.Subsystems.Intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FollowerType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
@@ -23,25 +22,25 @@ import frc.robot.Constants.PHConstants;
 public class IntakeSubsystem extends SubsystemBase {
   //Initializes solenoid and talon
   DoubleSolenoid m_intakePiston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PHConstants.IntakeForwardSolenoid, PHConstants.IntakeReverseSolenoid);
-  TalonFX m_intakeMotor = new TalonFX(CanConstants.IntakeMotor);
+  //TalonFX m_intakeMotor = new TalonFX(CanConstants.IntakeMotor);
   TalonFX m_intakeMotor2 = new TalonFX(CanConstants.IntakeMotor2);
   
   public static Boolean m_running;
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
-    m_intakeMotor.configFactoryDefault();
-    m_intakeMotor.configFactoryDefault();
-    m_intakeMotor.setNeutralMode(NeutralMode.Coast);
+    // m_intakeMotor.configFactoryDefault();
+    m_intakeMotor2.configFactoryDefault();
+    // m_intakeMotor.setNeutralMode(NeutralMode.Coast);
     m_intakeMotor2.setNeutralMode(NeutralMode.Coast);
     m_intakeMotor2.setInverted(true);
 
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 255);
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 255);
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 255);
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 255);
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_17_Targets1, 255);
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_9_MotProfBuffer, 255);
-    m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_17_Targets1, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_9_MotProfBuffer, 255);
+    // m_intakeMotor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 255);
 
     m_intakeMotor2.setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 255);
     m_intakeMotor2.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 255);
@@ -52,18 +51,18 @@ public class IntakeSubsystem extends SubsystemBase {
     m_intakeMotor2.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 255);
 
 
-    m_intakeMotor.configOpenloopRamp(0.75);
+    // m_intakeMotor.configOpenloopRamp(0.75);
     m_intakeMotor2.configOpenloopRamp(0.75);
 
-    m_intakeMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 15, 20, 0.10));
+    // m_intakeMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 15, 20, 0.10));
     m_intakeMotor2.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 15, 20, 0.10));
 
-    m_intakeMotor2.follow(m_intakeMotor, FollowerType.PercentOutput);
+    // m_intakeMotor2.follow(m_intakeMotor, FollowerType.PercentOutput);
 
     m_intakeMotor2.configPeakOutputForward(1.0);
-    m_intakeMotor.configPeakOutputForward(1.0);
+    // m_intakeMotor.configPeakOutputForward(1.0);
     m_intakeMotor2.configPeakOutputReverse(-1.0);
-    m_intakeMotor.configPeakOutputReverse(-1.0);
+    // m_intakeMotor.configPeakOutputReverse(-1.0);
 
     m_running = false;
   }
@@ -71,14 +70,14 @@ public class IntakeSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    if(m_intakeMotor.getMotorOutputPercent() > 0.1 || m_intakeMotor.getMotorOutputPercent() < -0.1){
+    if(m_intakeMotor2.getMotorOutputPercent() > 0.1 || m_intakeMotor2.getMotorOutputPercent() < -0.1){
       m_running = true;
     }
     else{
       m_running = false;
     }
     SmartDashboard.putBoolean("running", m_running);
-    SmartDashboard.putNumber("Intake Current Draw", m_intakeMotor.getSupplyCurrent());
+    SmartDashboard.putNumber("Intake Current Draw", m_intakeMotor2.getSupplyCurrent());
   }
 
 
@@ -90,7 +89,7 @@ public class IntakeSubsystem extends SubsystemBase {
    * @param speed speed to set intake motor at 
    */
   public void driveIntake(double speed){
-    m_intakeMotor.set(ControlMode.PercentOutput, speed * 1.0);
+    m_intakeMotor2.set(ControlMode.PercentOutput, speed * 1.0);
     //m_intakeMotor2.set(ControlMode.PercentOutput, speed *1.0);
     
  
